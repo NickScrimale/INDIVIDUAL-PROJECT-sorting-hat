29ee056e0c246365daf27b3376fbb09d9c53c9aa
NickScrimale <91919098+NickScrimale@users.noreply.github.com>
2022-04-30 12:14:36 -0500

    first commit

---
+++ data/students.js
@@ -0,0 +1,38 @@
+export const studentList = [
+    {
+        id: 1,
+        name: "Rennala",
+        house: "Gryffindor"
+    },
+    {
+        id: 2,
+        name: "Radahn",
+        house: "Slytherin"
+    },
+    {
+        id: 3,
+        name: "Sellen",
+        house: "Ravenclaw"
+    },
+    {
+        id: 4,
+        name: "Thops",
+        house: "Hufflepuff"
+    },
+    {
+        id: 5,
+        name: "Seluvis",
+        house: "Slytherin"
+    },
+    {
+        id: 6,
+        name: "Ranni",
+        house: "Gryffindor"
+    }
+]
+
+export const darkArmy = [
+    {
+        
+    }
+]
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+
+
+// for(var i = 0; i < array1.length; i++) {
+//     array2.push(array1[i]);
+//     array1.splice(i, 1);
+//     i--; //decrement i IF we remove an item
+//   }
+  
+//   console.log(array1); //[]
+//   console.log(array2); //[1, 2, 3, 4, 5]
